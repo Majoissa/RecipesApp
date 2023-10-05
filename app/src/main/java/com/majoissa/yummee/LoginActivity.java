d3646bd525c35de633e9136d340922af4a500cd7
María José Issa <95626214+Majoissa@users.noreply.github.com>
2023-10-05 22:59:12 +0200

    cambios hechos por tulio pero que su pc no le dejaba hacer los commits

--- app/src/main/java/com/majoissa/yummee/LoginActivity.java
+++ app/src/main/java/com/majoissa/yummee/LoginActivity.java
@@ -1,14 +1,74 @@
 package com.majoissa.yummee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewCreateAccount;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.editText);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.button);
+        textViewCreateAccount = findViewById(R.id.button2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Iniciar sesión con Firebase
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Inicio de sesión exitoso, redirigir a la actividad principal o realizar otras acciones
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // Ejemplo de redirección a una actividad principal
+                                    Intent intent = new Intent(LoginActivity.this, Main.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // Error en el inicio de sesión
+                                    Toast.makeText(LoginActivity.this, "Error en el inicio de sesión", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        textViewCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirigir a la actividad de registro
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
