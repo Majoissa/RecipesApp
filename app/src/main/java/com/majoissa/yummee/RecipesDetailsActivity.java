275385ee3c69d69c4f7392b4097d90945238ff95
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2023-10-05 08:09:11 +0200

    send coments_Activity

---
+++ app/src/main/java/com/majoissa/yummee/RecipesDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.majoissa.yummee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipesDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_details);
+    }
+}
