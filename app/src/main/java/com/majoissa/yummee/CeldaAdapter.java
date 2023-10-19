a0755710fb678c143934a5736726402c6850b5e7
Victoria <victoriaeso904@gmail.com>
2023-10-19 22:13:57 +0200

    Función de comentar y ver los comentarios añadido

--- app/src/main/java/com/majoissa/yummee/CeldaAdapter.java
+++ app/src/main/java/com/majoissa/yummee/CeldaAdapter.java
@@ -7,7 +7,13 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+<<<<<<< Updated upstream
 import com.bumptech.glide.Glide; // Importa Glide
+=======
+
+import com.squareup.picasso.Picasso;
+
+>>>>>>> Stashed changes
 import java.util.List;
 
 public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> {
@@ -28,6 +34,7 @@ public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Celda celda = celdas.get(position);
+<<<<<<< Updated upstream
         holder.titul.setText(celda.getRecipe_name());
 
         double calificacion = celda.getRating_recipes()/ 2.0;
@@ -38,6 +45,18 @@ public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> 
         Glide.with(holder.imatge.getContext())
                 .load(celda.getImg_url()) // La URL de la imagen
                 .into(holder.imatge);
+=======
+        Picasso.get().load(celda.getImg_url()).into(holder.imatge);
+        holder.titul.setText(celda.getRecipe_name());
+        holder.valoracio.setRating((float) celda.getRating_recipes());
+        holder.totalreviews.setText(celda.getTotalReviews());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Buttons.RecipeDetails(view);
+            }
+        });
+>>>>>>> Stashed changes
     }
 
     @Override
