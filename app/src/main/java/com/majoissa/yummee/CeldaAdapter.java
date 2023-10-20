3dd7b58b296a37d64e4cdd8eaeeb03e96156f7ac
Victoria <victoriaeso904@gmail.com>
2023-10-20 10:24:17 +0200

    Lo mismo que en el otro commit pero debería funcionar esta vez

--- app/src/main/java/com/majoissa/yummee/CeldaAdapter.java
+++ app/src/main/java/com/majoissa/yummee/CeldaAdapter.java
@@ -7,13 +7,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-<<<<<<< Updated upstream
-import com.bumptech.glide.Glide; // Importa Glide
-=======
-
 import com.squareup.picasso.Picasso;
-
->>>>>>> Stashed changes
 import java.util.List;
 
 public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> {
@@ -34,18 +28,6 @@ public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Celda celda = celdas.get(position);
-<<<<<<< Updated upstream
-        holder.titul.setText(celda.getRecipe_name());
-
-        double calificacion = celda.getRating_recipes()/ 2.0;
-        holder.valoracio.setRating((float) calificacion);
-        holder.totalreviews.setText(celda.getTotalReviews());
-
-        // Carga la imagen desde la URL utilizando Glide
-        Glide.with(holder.imatge.getContext())
-                .load(celda.getImg_url()) // La URL de la imagen
-                .into(holder.imatge);
-=======
         Picasso.get().load(celda.getImg_url()).into(holder.imatge);
         holder.titul.setText(celda.getRecipe_name());
         holder.valoracio.setRating((float) celda.getRating_recipes());
@@ -56,7 +38,6 @@ public class CeldaAdapter extends RecyclerView.Adapter<CeldaAdapter.ViewHolder> 
                 Buttons.RecipeDetails(view);
             }
         });
->>>>>>> Stashed changes
     }
 
     @Override
