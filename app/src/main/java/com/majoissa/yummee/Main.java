60c789cf0f36bd4faeea561d12ed94388f84ae52
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2023-10-23 13:00:25 +0200

    opensession

--- app/src/main/java/com/majoissa/yummee/Main.java
+++ app/src/main/java/com/majoissa/yummee/Main.java
@@ -1,60 +1,39 @@
 package com.majoissa.yummee;
 
-
 import android.content.Context;
 import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.os.Bundle;
-import android.view.View;
-
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 import java.util.List;
 
 //Esta es la clase
 public class Main extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private EditText searchEditText;
     private Button searchButton;
-    private Buttons buttons;
-    private ImageView user;
-    private ImageView favorites;
-    private FirebaseFirestore db;
-    private List<Celda> celdas;
-    private Celda celda;
-    private CeldaAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = FirebaseFirestore.getInstance();
-        buttons = new Buttons(this);
-        user = findViewById(R.id.imageView9);
-        favorites = findViewById(R.id.imageView2);
-
         recyclerView = findViewById(R.id.recyclerView);
         searchEditText = findViewById(R.id.searchEditText);
         searchButton = findViewById(R.id.searchButton);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -78,7 +57,7 @@ public class Main extends AppCompatActivity {
             public void onClick(View v) {
                 String query = searchEditText.getText().toString().trim();
                 if (!query.isEmpty()) {
-                    // Realiza la búsqueda con la cadena 'query'
+                    // Realiza la bÃºsqueda con la cadena 'query'
                 }
             }
         });
@@ -115,34 +94,10 @@ public class Main extends AppCompatActivity {
                         }
                         CeldaAdapter adapter = new CeldaAdapter(celdas);
                         recyclerView.setAdapter(adapter);
-        loadCeldas();
-        user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buttons.UserButton(view);
-            }
-        });
-    }
-
-    private void loadCeldas() {
-        // Realiza una consulta a Firestore para obtener los documentos de la colección "recetas"
-        db.collection("2023recipesApp").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-                if (documentSnapshot != null && !documentSnapshot.getDocuments().isEmpty()) {
-                    celdas = new ArrayList<>();
-                    List<DocumentSnapshot> documents = documentSnapshot.getDocuments();
-                    for (DocumentSnapshot value : documents) {
-                        String title = value.getString("recipe_name");
-                        String image = value.getString("img_url");
-                        String reviews = value.getString("totalReviews");
-                        double ratings = value.getDouble("rating_recipes");
-                        celdas.add(new Celda(title, image, ratings, reviews));
                     }
-                    adapter = new CeldaAdapter(celdas);
-                    recyclerView.setAdapter(adapter);
-                }
-            }
-        });
+                })
+                .addOnFailureListener(e -> {
+                    // Manejo de errores en caso de fallo en la consulta a Firestore
+                });
     }
 }
