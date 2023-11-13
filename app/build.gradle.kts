990e74fabcfaa4d4db65c6810ed9f4761b8b8992
Victoria <victoriaeso904@gmail.com>
2023-11-13 10:52:25 +0100

    Función de ver videos con links de YouTube agregado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
